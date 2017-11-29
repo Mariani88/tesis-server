ec13c71dc6df9c9258286a9e641fb9fd341ba49b
Mariani88 <pm_mariani@hotmail.com>
2017-11-28 23:00:32 -0300

    add tests structure

---
+++ src/test/kotlin/com/untref/tesis/server/action/ReceiveAlertTest.kt
@@ -0,0 +1,25 @@
+package com.untref.tesis.server.action
+
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+
+class ReceiveAlertTest {
+
+    lateinit var receiveAlert: ReceiveAlert
+
+    @Before
+    fun setUp() {
+        receiveAlert = ReceiveAlert()
+    }
+
+    @Test
+    fun receiveAlertShouldStoreAlert() {
+        fail()
+    }
+
+    @Test
+    fun receiveAlertShouldSendItToClient(){
+        fail()
+    }
+}
