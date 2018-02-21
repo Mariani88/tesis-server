a291885cfc92f28caa1102b0c615fe2d6b7a8ee1
Mariani88 <pm_mariani@hotmail.com>
2018-02-21 13:35:59 -0300

    refactor

--- src/test/kotlin/com/untref/tesis/server/unit/notification/infrastructure/FirebaseHeaderFactoryTest.kt
+++ src/test/kotlin/com/untref/tesis/server/unit/notification/infrastructure/FirebaseHeaderFactoryTest.kt
@@ -4,16 +4,34 @@ import com.untref.tesis.server.notification.infrastructure.FirebaseHeaderFactory
 import com.untref.tesis.server.notification.infrastructure.SERVER_KEY
 import org.junit.Assert
 import org.junit.Test
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 
 class FirebaseHeaderFactoryTest {
 
     private val authorization = "Authorization"
+    private lateinit var header: HttpHeaders
+    private lateinit var serverKey: String
 
     @Test
     fun headerShouldBeCreatedCorrectlyForFirebase() {
-        val header = FirebaseHeaderFactory.create()
+        givenAServerKey()
+
+        whenCreateHeader()
+
+        thenHeaderIsCreatedCorrectly()
+    }
+
+    private fun thenHeaderIsCreatedCorrectly() {
         Assert.assertEquals(MediaType.APPLICATION_JSON, header.contentType)
-        Assert.assertEquals(listOf(SERVER_KEY), header[authorization])
+        Assert.assertEquals(listOf(serverKey), header[authorization])
+    }
+
+    private fun whenCreateHeader() {
+        header = FirebaseHeaderFactory.create()
+    }
+
+    private fun givenAServerKey() {
+        serverKey = SERVER_KEY
     }
 }
