e644a39d8617a21264ad81b991c91c0ae9cf0ba2
Matias Mariani <pm_mariani@hotmail.com>
2018-04-04 23:15:27 -0300

    remove unused file

--- src/main/kotlin/com/untref/tesis/server/notification/infrastructure/FirebaseServer.kt
+++
@@ -1,9 +0,0 @@
-package com.untref.tesis.server.notification.infrastructure
-
-/*const val SERVER_KEY =
-        "key=AAAACIzVmn0:APA91bE7vROa0YlKwm54mawglb5i9VICp7RpH32RqY2pbRT2DOe3XhgWvtujOgjZYT0mBY8NPEHx4i4iC_mX9rIZxaaS5FjUqSAZEzfHKk9QRicoj7sKn2q" +
-                "-Je4zO0zqdaCP7jYARuXt"
-
-const val URL = "https://fcm.googleapis.com/fcm/send"
-
-const val TARGET = "/topics/alert"*/
